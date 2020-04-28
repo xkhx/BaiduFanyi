de91db2f74f83c1215ee890e6d7997d368f8a955
星空 <1838115958@qq.com>
2020-04-28 17:59:46 +0800

    init

---
+++ src/main/kotlin/me/xiaox/baidufanyi/BaiduFanyi.kt
@@ -0,0 +1,10 @@
+package me.xiaox.baidufanyi
+
+import net.mamoe.mirai.console.plugins.PluginBase
+
+class BaiduFanyi : PluginBase() {
+
+    override fun onEnable() {
+        
+    }
+}
---
+++ src/test/kotlin/me/xiaox/baidufanyi/HelloTest.kt
@@ -0,0 +1,8 @@
+package me.xiaox.baidufanyi
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
